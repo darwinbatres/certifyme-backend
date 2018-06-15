a9be818e90870eaae369224ef69805a7471bbeb3
Darwin Batres <darwin.ba3@gmail.com>
2018-06-14 18:24:15 -0600

    rename /coaching to /coach-practice

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ module.exports = (app) => {
   app.use('/api/v1/practices', practices);
   app.use('/api/v1/users', users);
   app.use('/api/v1/certifications', certifications);
-  app.use('/api/v1/coaching', coaching);
+  app.use('/api/v1/coach-practice', coaching);
 
   //   catch all route for non-existing requests
   app.all('*', (req, res) => {
