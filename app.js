a8bbc610a62fa3f895d243ef6f3f8445e864903f
Darwin Batres <darwin.ba3@gmail.com>
2018-06-14 22:11:25 -0600

    Start adding documenation for /users

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const routes = require('./routes');
 const { port } = require('./configuration');
 const database = require('./configuration/db');
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
+const swaggerUI = require('swagger-ui-express');
+
 const practices = require('./practices');
 const users = require('./users');
 const certifications = require('./certifications');
 const coaching = require('./coaching');
 
+const swaggerDocument = require('../configuration/swagger.json');
+
 module.exports = (app) => {
+  // setting up documentation
+  const options = {
+    swaggerOptions: {
+      explorer: true,
+      validatorUrl: null,
+    },
+  };
+  app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument, options));
+
+  // general routes
   app.use('/api/v1/practices', practices);
   app.use('/api/v1/users', users);
   app.use('/api/v1/certifications', certifications);
