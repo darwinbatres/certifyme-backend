5e39fbd931fca34856ef9ed9c4838ee5ec0a3b8a
Darwin Batres <batres.aguilar@gmail.com>
2018-06-15 19:04:55 +0000

    Start documentation

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Certifyme (backend)
 
 > backend application for the certify me POC
+
+## install
+
+- `npm install`
+
+## make sure environemt variables are configured
+
+- refer to `.env_ex`
+
+## API documentation
+
+- `/api-docs`
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,10 @@ const coaching = require('./coaching');
 const swaggerDocument = require('../configuration/swagger.json');
 
 module.exports = (app) => {
+  
+  console.log('path: ')
+  console.log(app);
+  console.log('end path')
   // setting up documentation
   const options = {
     swaggerOptions: {
@@ -18,7 +22,7 @@ module.exports = (app) => {
   app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument, options));
 
   // general routes
-  app.use('/api/v1/practices', practices);
+  // app.use('/api/v1/practices', practices);
   app.use('/api/v1/users', users);
   app.use('/api/v1/certifications', certifications);
   app.use('/api/v1/coach-practice', coaching);
